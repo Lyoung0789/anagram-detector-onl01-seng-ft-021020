6b5ebe00361eb80c462c38dc7c29fd5236bd60f5
Lester Young <lyoung070789@gmail.com>
2020-02-21 04:10:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -19,11 +19,8 @@ class Anagram
       
       if new_array == @word.split("").sort
         anagram_array << i
-        return anagram_array
       end 
-      
-    
-      
+      return anagram_array
     end 
     return anagram_array 
   end 

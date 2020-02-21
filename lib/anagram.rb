47d8c251ef600e2245e6a2341ae23a15fe275f09
Lester Young <lyoung070789@gmail.com>
2020-02-21 04:02:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,8 @@ class Anagram
   def match (array)
     anagram_array = []
     array.each do |i|
-      new_array= i.split("").sort
+      new_array = i.split("").sort
+      puts new_array
       
       if new_array == @word.split("").sort
         anagram_array << i

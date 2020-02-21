695ecac5c41ae1ce3c5f4ceaf27e8b63963b7a57
Lester Young <lyoung070789@gmail.com>
2020-02-21 04:08:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,16 +10,19 @@ class Anagram
     anagram_array = []
     array.each do |i|
       new_array = i.split("").sort
+      
       puts new_array
       puts "this is what"
+      
       new_word = @word.split("").sort
       puts new_word
+      
       if new_array == @word.split("").sort
         anagram_array << i
         return anagram_array
-      else 
-        return anagram_array
       end 
+      
+      return anagram_array
     
       
     end 
